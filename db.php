7e1b20e987a71d769673c42f9b8541100fb7d595
trh4ckn0n <trhacknon@proton.me>
2025-05-28 04:35:00 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = getenv("DB_HOST");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASS");
-$db   = getenv("DB_NAME");
+$host = getenv("DB_HOST") ?: "127.0.0.1"; // 👈 Forcer IP
+$user = getenv("DB_USER") ?: "root";
+$pass = getenv("DB_PASS") ?: "trkn";
+$db   = getenv("DB_NAME") ?: "trkn_lab";
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
